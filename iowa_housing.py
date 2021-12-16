49a07f31756ac33f661a593dbd89926b63887289
emanix <emanixworld@yahoo.com>
2021-12-16 16:45:55 +0100

    Added more files

--- iowa_housing.py
+++ iowa_housing.py
@@ -1,4 +1,8 @@
 #%%
+'''
+    Kaggle's Intro to Machine Learning
+'''
+
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
@@ -72,4 +76,66 @@ val_pred = rf_model.predict(val_X)
 rf_val_mae = mean_absolute_error(val_y, val_pred)
 
 print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
+# %%
+
+''' 
+    Intermediate Machine Learning 
+'''
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('./housing_price/train.csv', index_col='Id')
+X_test_full = pd.read_csv('./housing_price/test.csv', index_col='Id')
+
+# Obtain target and predictors
+y = X_full.SalePrice
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = X_full[features].copy()
+X_test = X_test_full[features].copy()
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+# %%
+X_train.head()
+# %%
+
+from sklearn.ensemble import RandomForestRegressor
+'''
+    Determine the best model to use
+'''
+
+# Define the models
+model_1 = RandomForestRegressor(n_estimators=50, random_state=0)
+model_2 = RandomForestRegressor(n_estimators=100, random_state=0)
+model_3 = RandomForestRegressor(n_estimators=100, criterion='mae', random_state=0)
+model_4 = RandomForestRegressor(n_estimators=200, min_samples_split=20, random_state=0)
+model_5 = RandomForestRegressor(n_estimators=100, max_depth=7, random_state=0)
+
+models = [model_1, model_2, model_3, model_4, model_5]
+
+from sklearn.metrics import mean_absolute_error
+
+# Function for comparing different models
+def score_model(model, X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
+    model.fit(X_t, y_t)
+    preds = model.predict(X_v)
+    return mean_absolute_error(y_v, preds)
+
+for i in range(0, len(models)):
+    mae = score_model(models[i])
+    print("Model %d MAE: %d" % (i+1, mae))
+# %%
+my_model = RandomForestRegressor(n_estimators=100, criterion='mae', random_state=0)
+my_model.fit(X, y)
+
+pred = my_model.predict(X_test)
+X_test.head()
+# %%
+# How to create submission file.
+submission = pd.DataFrame({'Id': X_test.index, 'SalePrice': pred})
+submission.to_csv('submission.csv', index=False)
+
 # %%
